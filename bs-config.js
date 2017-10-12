ba2c6ae5124f518833502635d78b006be467bf85
Mor Koshokaro <mor.k.747111@gmail.com>
2017-10-12 16:03:28 +0300

    Refactored the configuration so browserSync will detect .ts changes and call webpack to re-bundle with will call ts-loader that will compile the .ts files.

--- bs-config.js
+++ bs-config.js
@@ -1,17 +1,35 @@
-module.exports = function(bs) {
-	var npmRun = require("npm-run");
-	bs.watch("./src/**/*.ts", function(event, file) {
-		console.log(`Detected ${event} in .ts file ${file}`);
-		if (event === "change") {
-			npmRun.exec("npm run build", function(err, stdout, stderr) {
-                console.log("Done compiling. Reloading...")
-                bs.reload()
-            });
+// Taken from the example: https://github.com/Browsersync/recipes/tree/master/recipes/webpack.typescript
+
+module.exports = function(browserSync) {
+	var webpackDevMiddleware = require("webpack-dev-middleware");
+	var webpack = require("webpack");
+	var stripAnsi = require("strip-ansi");
+
+	var webpackConfig = require("./webpack.config");
+	var bundler = webpack(webpackConfig);
+
+	bundler.plugin("done", function(stats) {
+		if (stats.hasErrors() || stats.hasWarnings()) {
+			return browserSync.sockets.emit("fullscreen:message", {
+				title: "Webpack Error:",
+				body: stripAnsi(stats.toString()),
+				timeout: 100000
+			});
 		}
+		browserSync.reload();
 	});
 
 	return {
 		port: 3000,
-		files: ["./src/**/*.{html,htm,css,js,ts}"]
+		server: "dist",
+		logFileChanges: false,
+		middleware: [
+			webpackDevMiddleware(bundler, {
+				publicPath: webpackConfig.output.publicPath,
+				stats: { colors: true }
+			})
+		],
+		plugins: ["bs-fullscreen-message"],
+		files: ["dist/*.html", "dist/css/*.css"]
 	};
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-	entry: "./build/main.js",
+	entry: "./src/main.ts",
 	output: {
-		path: path.resolve(__dirname, "bin"),
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/bin",
 		filename: "bundle.js"
 	},
+	devtool: "#eval-source-map",
 	resolve: {
 		// Add '.ts' and '.tsx' as a resolvable extension.
 		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
